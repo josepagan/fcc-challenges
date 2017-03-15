c997c2d7aec530ed094f78e58ae290d012b73b90
Jose <jm.pagan@gmail.com>
2017-03-15 21:42:17 +0000

    factorialized finished

--- factorialize.js
+++ factorialize.js
@@ -1,6 +1,7 @@
-
-function factorialize(num) {
+function factorialize(num){
+  if (num > 1){
+    num = num*factorialize(num-1);
+  }
   return num;
 }
-
-factorialize(5);
+console.log(factorialize(5))
