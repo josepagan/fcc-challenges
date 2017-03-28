651b0bb803ff9242b6f0cb7ef290fea866c93bfa
Jose <jm.pagan@gmail.com>
2017-03-28 23:31:59 +0100

    findlongestword added, using reduce

---
+++ longestwordinstring.js
@@ -0,0 +1,14 @@
+
+function findLongestWord(str) {
+  var array = str.split(' ');
+  var max = array.reduce(function(previous,current){
+    if (current.length>previous.length) {
+          return current;
+        } else {
+          return previous;
+        }
+  });
+return max.length
+  }
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
