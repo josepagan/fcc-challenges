7b0cd4d8e212d29461127a1c2bcd8ed65f0446ba
Jose <jm.pagan@gmail.com>
2017-03-30 00:03:45 +0100

    added largestOfFour

--- largestOfFour.js
+++ largestOfFour.js
@@ -1,1 +1,19 @@
-a
+function largestOfFour(arr) {
+    var max = arr.map(function(val) {    //we itinerate over the four elements val is each subarray
+        return val.reduce(function(lot, next) { //we want to return the biggest in each, so we apply a reduce to val
+            if (lot > next) {
+                return lot;
+            } else {
+                return next;
+            }
+        });
+    });
+    return max;
+}
+
+largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+]);
