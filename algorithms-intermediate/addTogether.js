eb2347e76b0edf6eaeaf373cf3cd37d658d13523
Jose Pagan <jm.pagan@gmail.com>
2020-04-16 12:50:51 +0100

    more exercises done

---
+++ algorithms-intermediate/addTogether.js
@@ -0,0 +1,28 @@
+// Intermediate Algorithm Scripting: Arguments Optional
+// Create a function that sums two arguments together. If only one argument 
+// is provided, then return a function that 
+// expects one argument and returns the sum.
+
+// For example, addTogether(2, 3) should return 5, and addTogether(2)
+//  should return a function.
+
+// Calling this returned function with a single argument will then return
+//  the sum:
+
+// var sumTwoAnd = addTogether(2);
+
+// sumTwoAnd(3) returns 5.
+
+// If either argument isn't a valid number, return undefined.
+
+function addTogether() {
+    if ([...arguments].every(arg=>typeof(arg)==="number") === false) return undefined 
+    else if (arguments.length === 2) return arguments[0] + arguments[1]
+    else return (x)=>{
+        if (typeof(x)!=='number') return undefined
+        return x + arguments[0]} 
+  }
+  
+
+ console.log(addTogether(5)(2))
+  
